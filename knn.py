e3a3e7042a8e4dcaaa47142a41877398ed007f55
cvrcoe26 <guru02721@gmail.com>
2024-11-16 11:11:30 +0530

    Update knn.py

--- knn.py
+++ knn.py
@@ -1,10 +1,11 @@
-from sklearn.datasets import load_iris
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
-iris = load_iris()
-X = iris.data
-y = iris.target
+data = pd.read_csv("iris.csv")
+data['species'] = data['species'].map({'setosa': 0, 'versicolor': 1, 'virginica': 2})
+X = data[['sepal_length', 'sepal_width', 'petal_length', 'petal_width']]
+y = data['species']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 knn = KNeighborsClassifier(n_neighbors=3)
 knn.fit(X_train, y_train)
